1576e4100a590722f0f747d23c74c5831fbb78c7
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-05-11 17:06:17 +0200

    working on homepage

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutMeComponent } from './profile/about-me/about-me.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './profile/header/header.component';
+import { FooterComponent } from './profile/footer/footer.component';
+import { HomeComponent } from './profile/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutMeComponent,
-    ProfileComponent
+    ProfileComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
