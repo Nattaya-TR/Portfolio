1b37155eeff0bc17279c161d59ca9d34afe0bf45
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-05-12 17:11:01 +0200

    working on about me page

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { AboutMeComponent } from './profile/about-me/about-me.component';
+import {HeaderComponent} from './profile/header/header.component';
 
 const routes: Routes = [
-  {path: '', component: ProfileComponent}
+  {path: '', component: ProfileComponent},
+  {path: 'about', component: AboutMeComponent}
 ];
 
 @NgModule({
--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { AboutMeComponent } from './profile/about-me/about-me.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HeaderComponent } from './profile/header/header.component';
 import { FooterComponent } from './profile/footer/footer.component';
 import { HomeComponent } from './profile/home/home.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,9 @@ import { HomeComponent } from './profile/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
